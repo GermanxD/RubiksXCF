17250cc06a28bbb3e228b4252607fcf09d0f8568
German Bustos Moreno <52357914+GermanxD@users.noreply.github.com>
2023-09-11 08:57:26 -0600

    Agregar archivos de proyecto.

---
+++ RubiksMX/Controllers/CategoriasController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RubiksMX.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult _3x3()
+        {
+            return View();
+        }
+
+        public IActionResult _4x4()
+        {
+            return View();
+        }
+
+        public IActionResult _5x5()
+        {
+            return View();
+        }
+
+        public IActionResult _6x6()
+        {
+            return View();
+        }
+
+        public IActionResult _7x7()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RubiksMX/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+/* Scroll hacia abajo*/
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollButton = document.getElementById("scrollButton");
+
+    scrollButton.addEventListener("click", function () {
+        // Desplazarse hacia abajo
+        window.scrollTo({
+            top: window.innerHeight, // Altura de la ventana del navegador
+            behavior: "smooth"       // Desplazamiento suave
+        });
+    });
+});
